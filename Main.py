601ed72b2f05e1dde7dd23c1021835727f48a622
Simon Hostettler <simon.hostettler@protonmail.com>
2022-03-08 21:26:36 +0100

    randomized sampling of colors now available, faster than averaging entire frame

--- Main.py
+++ Main.py
@@ -3,31 +3,41 @@
 import sys
 import os
 import cv2
+import random
+import numpy as np
 import regex as re
+from tqdm import tqdm
 from tkinter.filedialog import askopenfilename
 from PIL import Image, ImageDraw
 
 
-def analyse_frames(movie_path, file_name):
-    # Opens video in cv2, gets frames each second, calculates average color of frame and writes it into file
+def analyse_frames(movie_path, file_name, randomized_selection=True):
+    '''Opens movie_path in cv2, gets frames each second, calculates average color of frame and writes it into file_name'''
+
     # how many frames to look at per second
-    FRAME_PER_SEC = 0.2
+    FRAME_PER_SEC = 1
 
-    with open(file_name, "w") as file:
-        avg_colors = []
+    avg_colors = []
 
-        video = cv2.VideoCapture(movie_path)
-        total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-        fps = int(video.get(cv2.CAP_PROP_FPS))
-        video.set(cv2.CAP_PROP_POS_MSEC, 0)
+    video = cv2.VideoCapture(movie_path)
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(video.get(cv2.CAP_PROP_FPS))
+    video.set(cv2.CAP_PROP_POS_MSEC, 0)
 
-        for i in progressbar(range(int(total_frames/(fps/FRAME_PER_SEC))), "Frames analyzed: "):
-            # only want one fps, skip the rest
-            for _ in range(0, int(fps/FRAME_PER_SEC)):
-                video.grab()
-            _, frame = video.retrieve()
+    for i in tqdm(range(int(total_frames/(fps/FRAME_PER_SEC))), "Frames analyzed: "):
+        # only want one fps, skip the rest
+        for _ in range(0, int(fps/FRAME_PER_SEC)):
+            video.grab()
+        _, frame = video.retrieve()
 
-            if frame is not None:
+        if frame is not None:
+            if randomized_selection:
+                avg_color_bgr = str(sample_avg_color(frame))[
+                    1:-1].strip()
+                avg_color_rgb = ",".join(
+                    reversed((re.sub("\s+", ",", avg_color_bgr)).split(",")))
+                avg_colors.append(avg_color_rgb)
+            else:
                 # average frame using numpy mean, convert it to string
                 avg_color_bgr = str(frame.mean(axis=(0, 1), dtype=int))[
                     1:-1].strip()
@@ -36,22 +46,35 @@ def analyse_frames(movie_path, file_name):
                     reversed((re.sub("\s+", ",", avg_color_bgr)).split(",")))
                 avg_colors.append(avg_color_rgb)
 
+    with open(file_name, "w") as file:
         file.write("\n".join(avg_colors))
 
 
+def sample_avg_color(frame):
+    total_size = frame.shape[0]*frame.shape[1]
+    num_samples = int(0.01*total_size)
+    frame = np.reshape(frame, (total_size, 3))
+
+    rand_vec = np.random.randint(
+        low=0, high=total_size, size=num_samples, dtype=int)
+
+    return frame[rand_vec].mean(axis=0, dtype=int)
+
+
 def file_len(file_name):
-    # returns line number of file
+    '''returns line number of file'''
     return sum(1 for line in open(file_name))
 
 
 def get_color_brightness(c):
-    # calculates perceived brightness of color by converting to greyscale, returns float from 0 - 1
+    '''calculates perceived brightness of color by converting to greyscale, returns float from 0 - 1'''
     return ((c[0] * 0.2989) + (c[1] * 0.5870) + (c[2] * 0.114))/255
 
 
 def create_barcode_poster(file_name):
-    # creates png with pixel height equal to the analysed frames, width is 2/3 of height (standard poster format)
-    # draws a horizontal line with color of every frame
+    '''creates png with pixel height equal to the analysed frames, width is 2/3 of height (standard poster format).
+    Then draws a horizontal line with color of every frame'''
+
     height = file_len(file_name)
     width = int(height / 1.5)
 
@@ -70,8 +93,8 @@ def create_barcode_poster(file_name):
 
 
 def create_wave_poster(file_name):
-    # creates png with pixel height equal to the analysed frames, width is 2/3 of height (standard poster format)
-    # draws a line each loop whose width depends on relative brightness of frame to create wave effect
+    '''creates png with pixel height equal to the analysed frames, width is 2/3 of height (standard poster format)
+    Then draws a line each loop whose width depends on relative brightness of frame to create wave effect'''
 
     # how much the brightness affects the length of a line, exponential
     BRIGHTNESS_COEFF = 0.3
@@ -118,8 +141,7 @@ def create_wave_poster(file_name):
 
 
 def create_average_poster(file_name):
-    # averages a number of frame then draws a polygon for each color, length depending on previous and following brightness
-
+    '''averages a number of frame then draws a polygon for each color, length depending on previous and following brightness'''
     # sets number of frames to average colors together
     NUM_FRAMES_GROUPED = 30
     # how much the brightness affects the length of a line, exponential
@@ -184,23 +206,6 @@ def resize_image(file, max_length):
     img.save(file)
 
 
-def progressbar(iterator, pre="", size=60, file=sys.stdout):
-    count = len(iterator)
-    def clear(): return os.system('clear')
-
-    def show(j):
-        x = int(size*j/count)
-        file.write("%s[%s%s] %i/%i\r" % (pre, "#"*x, "."*(size-x), j, count))
-        file.flush()
-    show(0)
-    for i, item in enumerate(iterator):
-        yield item
-        clear()
-        show(i+1)
-        file.write("\n")
-        file.flush()
-
-
 if __name__ == "__main__":
     if not os.path.exists(sys.path[0]+"/Images/"):
         os.makedirs(sys.path[0]+"/Images/")
@@ -212,7 +217,7 @@ if __name__ == "__main__":
 
     try:
         if not(os.path.isfile(sys.path[0]+"/"+file_name)):
-            analyse_frames(video_path, file_name)
+            analyse_frames(video_path, file_name, randomized_selection=True)
     except Exception as e:
         print(e)
     create_average_poster(file_name)
